ccb3afe4ee0437e033374ab0edc5c8c096612931
Patrick Smyth <psmyth1994@gmail.com>
2020-03-14 14:33:04 -0400

    debugging
    fixed the problem I was having with RcppArmadillo.skeleton.packages

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,7 +2,8 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <RcppArmadillo.h>
-//[[Rcpp::depends(RcppArmadillo)]]
+#include <Rcpp.h>
+
 using namespace Rcpp;
 
 // curve_fitting_c
